7b69356d35612296107eaf7590d0a96523c0f452
MarkusTomio <markus.tomio@outlook.com>
2026-06-06 21:17:39 +0200

    :feat: [WebDev] Add point registration and point layer
    - Two points are stored in variables before ending measuring
    - Source for point features is added
    - Two points are added to the source
    - Features stored in source are rendered client side
    - Rendering style is individualised for better visibility

--- webDev/mainmap.js
+++ webDev/mainmap.js
@@ -1,10 +1,35 @@
 let initialZoomLevel = 10;
 let initialCenter = [1588911.734653, 6026906.806230];
+// Define source of features for vector layer suited for editing
+let pointFeatures = new ol.source.Vector();
+// Define measuring state
+let measuring = false;
+// Define measuring points
+let measuringPoint1 = null;
+let measuringPoint2 = null;
 
 let mapObjectInput = {
         layers: [
           new ol.layer.Tile({
             source: new ol.source.OSM()
+          }),
+          // Vector data for client side rendering
+          // Takes our pointFeatures as source
+          new ol.layer.Vector({
+            source: pointFeatures,
+            // Change default style for better visibility
+            style: new ol.style.Style({
+                image: new ol.style.Circle({
+                    radius: 6,
+                    fill: new ol.style.Fill({
+                        color: 'red'
+                    }),
+                    stroke: new ol.style.Stroke({
+                    color: 'black',
+                    width: 2
+                    })
+                })
+            })
           })
         ],
         target: 'map',
@@ -15,8 +40,6 @@ let mapObjectInput = {
       };
 
 var map = new ol.Map(mapObjectInput);
-var measuring = false;
-var measuringPoints = [];
 
 document.getElementById('zoom-out').onclick = function() {
     var view = map.getView();
@@ -60,11 +83,31 @@ document.getElementById('down').onclick = function() {
 };
 
 map.on('click', function(e) {
-    console.log(e);
+    // If measuring is not active do nothing
+    if (!measuring) {
+        return;
+    }
+
+    // Define new point feature with last click coordinate
+    var measurePoints = new ol.Feature({
+        geometry: new ol.geom.Point(e.coordinate)
+    });
+    
+    // Add the feature to the point features source,
+    // which is then rendered client side (see MapObjectInput)
+    pointFeatures.addFeature(measurePoints);
+
+    // If first point is still null, assign it clicked coordinate
+    // If it's not null, we know first one is recorded and assign clicked coordinate to second one
+    if(measuringPoint1 == null) {
+        measuringPoint1 = e.coordinate;
+    } else {
+        measuringPoint2 = e.coordinate;
+        measuring = false;
+    }
 });
 
 document.getElementById('measure').onclick = function() {
     measuring = true;
-    measuringPoints = [];
     alert("Measurement mode: click two points on the map.");
 }
