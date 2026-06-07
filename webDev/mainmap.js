5fa84abe8a15333825a04ce73cf608b8688192de
MarkusTomio <markus.tomio@outlook.com>
2026-06-07 11:58:47 +0200

    :feat: [WebDev] Add geographic distance measurement and result display on page
    - add transformation of points
    - add sphere on which distance is calculated
    - add distance calculation
    - add status and result span
    - add status and result span messages accordingly

--- webDev/mainmap.js
+++ webDev/mainmap.js
@@ -5,8 +5,10 @@ let pointFeatures = new ol.source.Vector();
 // Define measuring state
 let measuring = false;
 // Define measuring points
-let measuringPoint1 = null;
-let measuringPoint2 = null;
+let measureP1 = null;
+let measureP2 = null;
+
+var distance;
 
 let mapObjectInput = {
         layers: [
@@ -44,13 +46,15 @@ var map = new ol.Map(mapObjectInput);
 document.getElementById('zoom-out').onclick = function() {
     var view = map.getView();
     var zoom = view.getZoom();
-    view.setZoom(zoom - 1);
+    view.animate({zoom: zoom - 1});    
+    //view.setZoom(zoom - 1);
 };
 
 document.getElementById('zoom-in').onclick = function() {
     var view = map.getView();
     var zoom = view.getZoom();
-    view.setZoom(zoom + 1);
+    view.animate({zoom: zoom + 1}); 
+    //view.setZoom(zoom + 1);
 };
 
 document.getElementById('reset').onclick = function() {
@@ -99,15 +103,53 @@ map.on('click', function(e) {
 
     // If first point is still null, assign it clicked coordinate
     // If it's not null, we know first one is recorded and assign clicked coordinate to second one
-    if(measuringPoint1 == null) {
-        measuringPoint1 = e.coordinate;
+    if(measureP1 == null) {
+        measureP1 = e.coordinate;
+        return;
     } else {
-        measuringPoint2 = e.coordinate;
+        measureP2 = e.coordinate;
+
+        // Transform points to geographic coordinates, i.e. lonlat
+        var measureP1geo = ol.proj.transform (
+            measureP1,
+            'EPSG:3857',
+            'EPSG:4326'
+        );
+
+        var measureP2geo = ol.proj.transform (
+            measureP2,
+            'EPSG:3857',
+            'EPSG:4326'
+        );
+
+        // Define sphere on which to measure the distance
+        // From ol.Sphere docs: radius equal to semi-major axis of WGS84 ellipsoid
+        var sphere = new ol.Sphere(6378137);
+
+        // Calculate haversine distance between points
+        distance = sphere.haversineDistance(measureP1geo, measureP2geo);
+
+        // Set status to normal
         measuring = false;
+        // Set points to null for subsequent measurement
+        measureP1 = null;
+        measureP2 = null;
+
+        // Display measurement results on page
+        document.getElementById('measureStatus').innerHTML = 
+        "Geographical distance between first and second point is measured at: " + 
+        Math.floor(distance/1000) + "km " + 
+        (distance%1000).toFixed(2) + "m (" +
+        distance.toFixed(2) + "m).";
+        return;
     }
 });
 
 document.getElementById('measure').onclick = function() {
+    // Initiate measurement status
     measuring = true;
-    alert("Measurement mode: click two points on the map.");
+    // Empty features source, i.e. from previous measurement
+    pointFeatures.clear();
+    // Display measurement status and instructions on page
+    document.getElementById('measureStatus').innerHTML = "Measurement mode: click two points on the map.";
 }
